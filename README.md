aacd65bb3765debbb16e8b31f77883fb06a5b412
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2024-05-17 22:35:12 -0700

    Use huggingface model for zero shot classification

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Machine Learning Project with News Headlines
 
 - Install [Python](http://python.org) version 3.8 or above
-- Install dependencies `pip install scikit-learn`
+- Install dependencies `pip3 install transformers scikit-learn tqdm`
 - Download `Sarcasm_Headlines_Dataset_v2.json` from [Kaggle](https://www.kaggle.com/datasets/rmisra/news-headlines-dataset-for-sarcasm-detection/).
 - Run `python3 main.py`
--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import json
 
-def main():
-    v1 = set()
-    for line in open('Sarcasm_Headlines_Dataset.json'):
-        d = json.loads(line)
-        v1.add(d['headline'])
+from transformers import pipeline
+from tqdm import tqdm
 
-    v2 = set()
+def get_model_classifications(texts, labels=['not sarcastic', 'sarcastic'], batch_size=20):
+    classifier = pipeline(
+        "zero-shot-classification",
+        model="sileod/deberta-v3-base-tasksource-nli"
+    )
+    res = []
+    it = tqdm(
+        range(len(texts) // batch_size + 1),
+        desc=f'Processing batches of size {batch_size}'
+    )
+    for batch_idx in it:
+        text_batch = texts[batch_idx * batch_size: (batch_idx + 1) * batch_size]
+        clses = classifier(text_batch, labels)
+        res += [
+            d['labels'][
+                max(enumerate(d['scores']), key=lambda ix: (ix[1], ix[0]))[0]
+            ]
+            for d in clses
+        ]
+    return res
+
+def main():
+    input_strs = []
+    targets = []
     for line in open('Sarcasm_Headlines_Dataset_v2.json'):
         d = json.loads(line)
-        v2.add(d['headline'])
-
-    print(v2 - v1)
+        input_strs.append(d['headline'])
+        targets.append(d['is_sarcastic'])
+    results = get_model_classifications(input_strs)
+    print(f'Model guesses: {results}')
+    print(f'True answers: {targets}')
 
 if __name__ == '__main__':
     main()
