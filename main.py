14580ef275b79d918c36c7bbe2c4e56653fcc36e
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2024-05-26 19:06:42 -0700

    Save model predictions to pickle file

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
 import json
+from pathlib import Path
+import pickle
 
 import numpy as np
 from transformers import pipeline
 from tqdm import tqdm
 
 def get_model_classifications(texts, labels=['not sarcastic', 'sarcastic'], batch_size=20):
-    classifier = pipeline(
-        "zero-shot-classification",
-        model="sileod/deberta-v3-base-tasksource-nli"
+    saved_file = Path(
+        f'saved_classifications_num-{len(texts)}_labels-'
+        f"{'-vs-'.join([s.replace(' ', '-') for s in labels])}"
+        '.pkl'
     )
-    res = []
-    it = tqdm(
-        range(len(texts) // batch_size + 1),
-        desc=f'Processing batches of size {batch_size}'
-    )
-    for batch_idx in it:
-        text_batch = texts[batch_idx * batch_size: (batch_idx + 1) * batch_size]
-        clses = classifier(text_batch, labels)
-        res += [
-            d['labels'][
-                max(enumerate(d['scores']), key=lambda ix: (ix[1], ix[0]))[0]
+    if not saved_file.exists():
+        classifier = pipeline(
+            "zero-shot-classification",
+            model="sileod/deberta-v3-base-tasksource-nli"
+        )
+        res = []
+        it = tqdm(
+            range(len(texts) // batch_size + 1),
+            desc=f'Processing batches of size {batch_size}'
+        )
+        for batch_idx in it:
+            text_batch = texts[batch_idx * batch_size: (batch_idx + 1) * batch_size]
+            clses = classifier(text_batch, labels)
+            res += [
+                d['labels'][
+                    max(enumerate(d['scores']), key=lambda ix: (ix[1], ix[0]))[0]
+                ]
+                for d in clses
             ]
-            for d in clses
-        ]
+        pickle.dump(res, saved_file.open(mode='wb'))
+    else:
+        print(f'Loading classifications from {str(saved_file)}')
+        res = pickle.load(saved_file.open(mode='rb'))
     return res
 
 def main():
@@ -32,9 +44,11 @@ def main():
         d = json.loads(line)
         input_strs.append(d['headline'])
         targets.append(d['is_sarcastic'])
-    results = get_model_classifications(input_strs)
+    num_samples = len(input_strs)
+    results = get_model_classifications(input_strs[: num_samples])
+    breakpoint()
     predictions = np.array(results) == 'sarcastic'
-    print(f'Accuracy: {(predictions == targets).mean() * 100:.3f}%')
+    print(f'Accuracy: {(predictions == targets[: num_samples]).mean() * 100:.3f}%')
 
 if __name__ == '__main__':
     main()
